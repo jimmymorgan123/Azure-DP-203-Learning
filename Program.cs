614a4deed0c7b2b836facfc68d0c468615741f8a
jimmymorgan123 <88373454+jimmymorgan123@users.noreply.github.com>
2021-09-29 12:23:35 +0100

    Update Program.cs
    V.02

--- Program.cs
+++ Program.cs
@@ -1,8 +1,7 @@
 using System;
 using Microsoft.Extensions.Configuration;
 using System.IO;
-// Azure Storage Blobs client library for .NET
-using Azure.Storage.Blobs; 
+using Azure.Storage.Blobs;
 
 namespace PhotoSharingApp
 {
@@ -10,18 +9,34 @@ namespace PhotoSharingApp
     {
         static void Main(string[] args)
         {
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json");
-            //get the Azure Storage connection string and create the BlobContainerClient object
-            var configuration = builder.Build();
+        var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json");
 
-            var connectionString = configuration.GetConnectionString("StorageAccount");
-            string containerName = "photos";
+        var configuration = builder.Build();
 
-            BlobContainerClient container = new BlobContainerClient(connectionString, containerName);
-            
-            container.CreateIfNotExists();
+        Console.WriteLine("Hello World!");
+
+        // Get a connection string to our Azure Storage account.
+        var connectionString = configuration.GetConnectionString("StorageAccount");
+
+        // Get a reference to the container client object so you can create the "photos" container
+        string containerName = "photos";
+        BlobContainerClient container = new BlobContainerClient(connectionString, containerName);
+        container.CreateIfNotExists();
+
+        // Uploads the image to Blob storage.  If a blb already exists with this name it will be overwritten
+        string blobName = "docs-and-friends-selfie-stick";
+        string fileName = "docs-and-friends-selfie-stick.png";
+        BlobClient blobClient = container.GetBlobClient(blobName);
+        blobClient.Upload(fileName, true);
+
+        // List out all the blobs in the container
+        var blobs = container.GetBlobs();
+        foreach (var blob in blobs)
+        {
+            Console.WriteLine($"{blob.Name} --> Created On: {blob.Properties.CreatedOn:yyyy-MM-dd HH:mm:ss}  Size: {blob.Properties.ContentLength}");
         }
+    }
     }
 }
